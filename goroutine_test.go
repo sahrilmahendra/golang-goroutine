ebd843da976c762ecacf971a5cd906093057f36d
sahrilmahendra <moch.mahendra@algo.co.id>
2022-06-26 15:29:52 +0700

    Proofing Goroutine is Mild

--- goroutine_test.go
+++ goroutine_test.go
@@ -10,8 +10,22 @@ func RunGoroutine() {
 	fmt.Println("Hello World")
 }
 
+// membuat goroutine
 func TestGoroutine(t *testing.T) {
 	go RunGoroutine()
 	fmt.Println("Ups")
 	time.Sleep(1 * time.Second)
+}
+
+// membuktikan goroutine sangat ringan
+func DisplayNumber(number int) {
+	fmt.Println("Display ", number)
+}
+
+func TestManyGoroutine(t *testing.T) {
+	for i := 1; i <= 100000; i++ {
+		go DisplayNumber(i)
+	}
+
+	time.Sleep(10 * time.Second)
 }
