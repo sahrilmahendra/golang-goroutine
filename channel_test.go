e31e0cdee5580d1268a3c07e93d3646176f0d8ff
sahrilmahendra <moch.mahendra@algo.co.id>
2022-06-26 16:46:58 +0700

    Channel as Parameter

--- channel_test.go
+++ channel_test.go
@@ -20,3 +20,20 @@ func TestChannel(t *testing.T) {
 	fmt.Println(data)
 	time.Sleep(5 * time.Second)
 }
+
+func GiveMeResponse(channel chan string) {
+	time.Sleep(2 * time.Second)
+	channel <- "Moch. Syahryil Mahendra"
+	fmt.Println("Data telah dikirim")
+}
+
+func TestChannelAsParameter(t *testing.T) {
+	testChannel := make(chan string)
+	defer close(testChannel)
+
+	go GiveMeResponse(testChannel)
+
+	data := <-testChannel
+	fmt.Println(data)
+	time.Sleep(3 * time.Second)
+}
