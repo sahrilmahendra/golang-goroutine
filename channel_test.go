14def329aee0009266ba81784d1ab411d340a6ef
sahrilmahendra <moch.mahendra@algo.co.id>
2022-07-03 17:55:08 +0700

    Select Channel

--- channel_test.go
+++ channel_test.go
@@ -96,3 +96,27 @@ func TestRangeChannel(t *testing.T) {
 	}
 	fmt.Println("DONE")
 }
+
+// select channel
+func TestSelectChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+
+	go GiveMeResponse(channel1)
+	go GiveMeResponse(channel2)
+
+	counter := 0
+	for {
+		select {
+		case data := <-channel1:
+			fmt.Println("Data dari channel 1", data)
+			counter++
+		case data := <-channel2:
+			fmt.Println("Data dari channel 2", data)
+			counter++
+		}
+		if counter == 2 {
+			break
+		}
+	}
+}
