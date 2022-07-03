f624ab6957fbb1eca99271ad105846aee1b0ae33
sahrilmahendra <moch.mahendra@algo.co.id>
2022-07-03 17:18:45 +0700

    Add Buffered Channel

--- channel_test.go
+++ channel_test.go
@@ -58,3 +58,23 @@ func TestInOutChannel(t *testing.T) {
 
 	time.Sleep(5 * time.Second)
 }
+
+// buffered channel
+func TestBufferedChannel(t *testing.T) {
+	channel := make(chan string, 3)
+	defer close(channel)
+
+	go func() {
+		channel <- "Moch."
+		channel <- "Syahryil"
+		channel <- "Mahendra"
+	}()
+
+	go func() {
+		fmt.Println(<-channel)
+		fmt.Println(<-channel)
+		fmt.Println(<-channel)
+	}()
+
+	time.Sleep(2 * time.Second)
+}
