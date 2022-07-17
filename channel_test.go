1b7d956ced05ac72f410f7ad96f8ab4cb12e3cad
Moch Syahryil Mahendra <moch.mahendra@algo.co.id>
2022-07-17 16:48:55 +0700

    Default Select Channel

--- channel_test.go
+++ channel_test.go
@@ -120,3 +120,29 @@ func TestSelectChannel(t *testing.T) {
 		}
 	}
 }
+
+// default select channel
+func TestDefaultSelectChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+
+	go GiveMeResponse(channel1)
+	go GiveMeResponse(channel2)
+
+	counter := 0
+	for {
+		select {
+		case data := <-channel1:
+			fmt.Println("Data dari channel 1", data)
+			counter++
+		case data := <-channel2:
+			fmt.Println("Data dari channel 2", data)
+			counter++
+		default:
+			fmt.Println("Menunggu data")
+		}
+		if counter == 2 {
+			break
+		}
+	}
+}
