93671e8e15b071fe92c96115069d87e0c605bc05
Moch Syahryil Mahendra <moch.mahendra@algo.co.id>
2022-07-17 18:55:06 +0700

    Mutex * for prevent race condition

--- mutex_test.go
+++ mutex_test.go
@@ -2,17 +2,21 @@ package golanggoroutine
 
 import (
 	"fmt"
+	"sync"
 	"testing"
 	"time"
 )
 
-// race condition
-func TestRaceCondition(t *testing.T) {
+// mutex
+func TestMutex(t *testing.T) {
 	x := 0
+	var mutex sync.Mutex
 	for i := 1; i <= 1000; i++ {
 		go func() {
 			for j := 1; j <= 100; j++ {
+				mutex.Lock()
 				x = x + 1
+				mutex.Unlock()
 			}
 		}()
 	}
